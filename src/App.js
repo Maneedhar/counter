4788222f74331a3cf47b414ebf6aefcb95e6756b
Maneedhar <maneedhar@ideatostartup.org>
2021-06-20 23:39:50 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState({ val: 1, maxVal: 1000 });
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: parseInt(e.target.value) });
+  };
+
+  const increment = () => {
+    if (input.val >= input.maxVal) {
+      setError(true);
+    } else {
+      setInput({ ...input, val: input.val + 1 });
+      setError(false);
+    }
+  };
+
+  const decrement = () => {
+    if (input.val > input.maxVal) {
+      setError(true);
+    } else {
+      setInput({ ...input, val: input.val - 1 });
+      setError(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label>Max Value</label>
+        <input
+          type="number"
+          name="maxVal"
+          value={input.maxVal}
+          className="max"
+          placeholder="Enter a max value"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="counter">
+        <div className="minus btn" onClick={decrement}>
+          -
+        </div>
+        <input
+          type="number"
+          name="val"
+          max={1000}
+          value={input.val}
+          onChange={handleChange}
+          className="value"
+          disabled={error}
+        />
+        <div className="plus btn" onClick={increment}>
+          +
+        </div>
+      </div>
+      <div className="error">{error && 'Max limit reached'}</div>
     </div>
   );
 }
